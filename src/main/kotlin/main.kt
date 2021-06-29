c87c3f9c9d74325add83901a5bd4ee3fb9807c40
donfiazo <donfiazo@gmail.com>
2021-06-29 07:16:13 +0100

    Logic Assessment Commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,11 @@
+fun evenlySpaced(a: Int, b: Int, c: Int): Boolean {
+    return ((a+b==2*c) || (a+c==2*b) || (b+c==2*a))
+    }
+
+
+fun main(){
+    println(evenlySpaced(2, 4, 6))
+    println(evenlySpaced(4, 6, 2))
+    println(evenlySpaced(4, 6, 3))
+
+}
